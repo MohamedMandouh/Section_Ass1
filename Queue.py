4d6ae7f875ba903500e7a3ff813bc9fe190bbd09
MohamedMandouh <Mohamedmandouh978@gmail.com>
2019-10-16 23:30:16 +0200

    Queue/Stack python

--- Queue.py
+++ Queue.py
@@ -1,17 +1,23 @@
+class stack :
+    i = []
+    def stackout(self) :
+        self.i.pop(0)   
+    def stackin(self , f) :
+        self.i.append(f)
+
 def main() :
-    x=[]
-    while x :
-        print(x)
+    x = stack()
+    while True :
+        print(x.i)
         print( 'choose what to do in the queue    1- insert 2- remove 3- end the program')
         decision = (input('enter you choice ....'))
-        
         if decision == '1':
-            x.append(input("pleast enter an element to the insert")) 
+            x.stackin(input("pleast enter an element to insert")) 
         elif decision == '2':
-            if len(x) == 0:
+            if len(x.i) == 0:
               print("list is clear .... you must enter elements in the list first")
             else:
-               x.pop(0)
+               x.stackout()
         elif decision == '3':
             break
             
